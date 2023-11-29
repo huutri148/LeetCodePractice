68a79d2de57a9fccce1d8f1c3a5df2f57ddd3a47
huutri148 <huutri148@gmail.com>
2023-11-29 21:22:50 +0700

    fix: refactor code

--- array-hashing/35-search-insert-position/index.js
+++ array-hashing/35-search-insert-position/index.js
@@ -9,20 +9,17 @@
  * @return {number}
  */
 
-
 // Using Binary Search with O(log n)
 var searchInsert = function (nums, target) {
-  let leftIndex = 0;
-  let rightIndex = nums.length - 1;
+  let leftIndex = 0,
+    rightIndex = nums.length - 1;
 
   while (leftIndex <= rightIndex) {
     let middleIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
 
     if (target == nums[middleIndex]) return middleIndex;
     else if (nums[middleIndex] < target) leftIndex = middleIndex + 1;
-    else {
-      rightIndex = middleIndex - 1;
-    }
+    else rightIndex = middleIndex - 1;
   }
 
   return leftIndex;
