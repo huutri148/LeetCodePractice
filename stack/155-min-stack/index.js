bbcac798d6469a4cc964238862b96faf2eca3c39
huutri148 <huutri148@gmail.com>
2023-12-10 09:05:38 +0700

    feat: adding nodemon

---
+++ stack/155-min-stack/index.js
@@ -0,0 +1,53 @@
+var MinStack = function () {
+  this.stackArr = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  let item;
+  const arrLength = this.stackArr.length;
+
+  if (arrLength === 0) {
+    item = { val, min: val };
+  } else if (arrLength > 0) {
+    item = {
+      val,
+      min: Math.min(this.stackArr[arrLength - 1].min, val),
+    };
+  }
+
+  this.stackArr.push(item);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  const val = this.stackArr.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stackArr[this.stackArr.length - 1].val;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stackArr[this.stackArr.length - 1].min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
