0196c8c1a972777b57d55e3fc124ed4b7c36e3da
huutri148 <huutri148@gmail.com>
2023-12-13 21:01:17 +0700

    feat: adding 13/12/2023

--- stack/22-generate-parenthese/index.js
+++ stack/22-generate-parenthese/index.js
@@ -1,7 +1,23 @@
 /**
  * https://leetcode.com/problems/generate-parentheses/
+ * Time O(4*N/sqrt(n)) | Space O(n)
  * @param {number} n
  * @return {string[]}
  */
 
-const generateParenthesis = (n) => {};
+const generateParenthesis = (n) => {
+  const res = [];
+
+  const generateRes = (left, right, str) => {
+    if (str.length === 2 * n) {
+      res.push(str);
+      return;
+    }
+
+    if (left < n) generateRes(left + 1, right, str + "(");
+    if (right < left) generateRes(left, right + 1, str + ")");
+  };
+
+  generateRes(0, 0, "");
+  return res;
+};
