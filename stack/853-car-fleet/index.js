06aefbcbaaaaee1504ca53560398536f77f4c57c
huutri148 <huutri148@gmail.com>
2023-12-16 08:13:29 +0700

    feat: adding 16/12/2023

---
+++ stack/853-car-fleet/index.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/car-fleet/
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+const carFleet = (target, position, speed) => {};
